c8aa5559d8e63f41e9f47563dc22990b50cff279
zeruFili <zerubabelfilmon1@gmail.com>
2025-02-21 16:56:31 +0300

    Created product and cart functionality

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import {connectDB} from "./lib/db.js"; 
+import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000; 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
 
 
 app.listen(PORT, () => {
