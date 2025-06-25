c6e9b91ffe16d59083b751fc0bd49b64bb4000fc
zeruFili <zerubabelfilmon1@gmail.com>
2025-06-26 00:45:31 +0300

    Resolved merge conflicts in server.js

--- server.js
+++ server.js
@@ -6,32 +6,24 @@ const ApiError = require('./utils/ApiError'); // Adjust the path as necessary
 const httpStatus = require('http-status');
 const {successHandler, errorHandlers} = require('./config/morgan');  
 const productRouter = require('./routes/product.route');
-<<<<<<< HEAD
-const cartRouter = require('./routes/cart.route');
- 
-=======
+
 const cookieParser = require("cookie-parser");
 const cartRouter = require('./routes/cart.route');
 const paymentRouter = require('./routes/payment.route');
->>>>>>> origin/payment
+
 app.use(successHandler);
 app.use(errorHandlers);
 
 // Middleware to parse JSON
 app.use(express.json());
-<<<<<<< HEAD
 
-=======
 app.use(cookieParser());
->>>>>>> origin/payment
+
 // Define API routes
 app.use('/api/auth', authRouter);
 app.use('/api/product', productRouter);
 app.use('/api/cart', cartRouter);
-<<<<<<< HEAD
-=======
 app.use('/api/payment', paymentRouter);
->>>>>>> origin/payment
 
 
 // Handle unknown routes
